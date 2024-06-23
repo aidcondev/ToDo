03cd4e2bdee83c11ccbf67188bf62c17ae47c2b7
aidancon <aidan34connolly@gmail.com>
2024-06-23 17:27:45 -0400

    starting app

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// const name = 'Aidan';
+// const age = 2;
+// let message = `my name is ${name}` + " And I am " + age + " years old!";
+
+// console.log(message);
+
+// const lovesJavascript = true
+
+// if (lovesJavascript) {
+//     console.log(name + "loves javascript?")
+// }
+
+const tasks = []
+
+function addTask () {
+    let taskInput = document.getElementById("new-task");
+    console.log(taskInput);
+    let taskText = taskInput.ariaValueMax.trim();
+}
